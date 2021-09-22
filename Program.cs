5fd7482cc542b7f2ec212297aba03f13b1a7a174
LTIUser <LTIUser@INFEDL31192.lntinfotech.com>
2021-09-22 11:55:04 -0500

    Initialized repository and files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CarDealershipConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
